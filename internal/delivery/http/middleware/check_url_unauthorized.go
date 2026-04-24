f53c8c11fc2d1c0e83dfda721d6bc9facd9ad1fa
faldiega <naufaldi.egaputra@finansia.com>
2026-04-25 01:53:10 +0700

    reset the check_url_unauthorized.go

--- internal/delivery/http/middleware/check_url_unauthorized.go
+++ internal/delivery/http/middleware/check_url_unauthorized.go
@@ -1,14 +1,3 @@
 package middleware
 
-import "strings"
-
-// function ini mengembalikan true jika URL tidak butuh JWT
-func IsUnauthorizedURL(requestPath string, whitelist []string) bool {
-	for _, url := range whitelist {
-		// exact match
-		if strings.EqualFold(requestPath, url) {
-			return true
-		}
-	}
-	return false
-}
+// delete for a while because this changes in the wrong branch
